55d988d0b81e03ca433abd62e6f58604f2ac3a00
Colin Dismuke <colin.dismuke@gmail.com>
2019-08-18 20:54:30 -0500

    Added valid_proof method to miner

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -39,6 +39,18 @@ def valid_proof(last_hash, proof):
     IE:  last_hash: ...999123456, new hash 123456888...
     """
 
+    last_hash = str(last_hash).encode()
+    proof = str(proof).encode()
+    
+    # build string to hash
+    last_guess = hashlib.sha256(last_hash).hexdigest()
+    guess_hash = hashlib.sha256(proof).hexdigest()
+
+    # use hash function and check if 6 leading 0's in hash result
+    beginning = str(guess_hash[:6])
+    end = str(last_guess[-6:])
+
+    return beginning == end
 
 if __name__ == '__main__':
     # What node are we interacting with?
