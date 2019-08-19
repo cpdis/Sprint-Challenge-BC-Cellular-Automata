3989188749df2b37e3115a80814b3987d68312ec
Colin Dismuke <colin.dismuke@gmail.com>
2019-08-18 21:20:43 -0500

    Added logic for Rule 126

--- ca/1d-life.py
+++ ca/1d-life.py
@@ -8,6 +8,34 @@ def get_new_value(old_gen, old_automata):
         e = i + 1
         w = i - 1
         s = ((SQ_NUM * old_gen) + 1) + 49
+
+        if s < len(automata):
+            if current_row[i] == 1:
+                if e < len(current_row):
+                    if current_row[e] == 1 and current_row[w] == 1:
+                        automata[s] = 0
+                    else:
+                        automata[s] = 1
+                else:
+                    if current_row[w] == 1:
+                        automata[s] = 0
+                    else:
+                        automata[s] = 1
+            else:
+                if e < len(current_row):
+                    if current_row[e] == 0 and current_row[w] == 0:
+                        automata[s] = 0
+                    else:
+                        automata[s] = 1
+                else:
+                    if current_row[w] == 0:
+                        automata[s] = 0
+                    else:
+                        automata[s] = 1
+            
+            if i == 0 or i == (len(current_row) - 1):
+                if current_row[i] == 1:
+                    automata[s] = 1
         
     return automata
 
